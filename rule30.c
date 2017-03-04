e50dd91a6f974eacab964ceeef500bcd953a70c2
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-03-04 23:28:00 +0900

    Added eca_27()

--- rule30.c
+++ rule30.c
@@ -515,6 +515,19 @@ inline static void eca_26(const struct bitfield *left, const struct bitfield *ce
 	bfdel(tmp3);
 }
 
+inline static void eca_27(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
+{
+	struct bitfield *tmp1 = bfnot(center);
+	struct bitfield *tmp2 = bfxor(left, tmp1);
+	struct bitfield *tmp3 = bfor(tmp2, right);
+	struct bitfield *tmp4 = bfxor(left, tmp3);
+	bfcpy(tmp4, output);
+	bfdel(tmp1);
+	bfdel(tmp2);
+	bfdel(tmp3);
+	bfdel(tmp4);
+}
+
 inline static void eca_30(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
 {
 	struct bitfield *tmp1 = bfor(center, right);
@@ -609,6 +622,9 @@ void eca_string_ip(struct bitfield *instance, const unsigned int wolfram_code)
 			break;
 		case 26:
 			eca_26(left, center, right, instance);
+			break;
+		case 27:
+			eca_27(left, center, right, instance);
 			break;
 		case 30:
 			eca_30(left, center, right, instance);
@@ -708,6 +724,9 @@ struct bitfield *eca_string(const struct bitfield *input, const unsigned int wol
 			break;
 		case 26:
 			eca_26(left, center, right, output);
+			break;
+		case 27:
+			eca_27(left, center, right, output);
 			break;
 		case 30:
 			eca_30(left, center, right, output);
