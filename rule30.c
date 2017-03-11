0bb7f1ac856f00496e6ceff4e473c29279723e22
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-03-11 23:04:00 +0900

    Added eca_45()

--- rule30.c
+++ rule30.c
@@ -732,7 +732,13 @@ inline static void eca_44(const struct bitfield *left, const struct bitfield *ce
 
 inline static void eca_45(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
 {
-
+	struct bitfield *tmp1 = bfnot(right);
+	struct bitfield *tmp2 = bfor(center, tmp1);
+	struct bitfield *tmp3 = bfxor(left, tmp2);
+	bfcpy(tmp3, output);
+	bfdel(tmp1);
+	bfdel(tmp2);
+	bfdel(tmp3);
 }
 
 inline static void eca_46(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
