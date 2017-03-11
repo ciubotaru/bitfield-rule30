62c4da68d541f4db62abf280792811172ffeb483
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-03-11 23:47:00 +0900

    Added eca_88()

--- rule30.c
+++ rule30.c
@@ -1238,7 +1238,14 @@ inline static void eca_87(const struct bitfield *left, const struct bitfield *ce
 
 inline static void eca_88(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
 {
-
+	/* 1 XOR ((1 OR 2) AND 3) */
+	struct bitfield *tmp1 = bfor(left, center);
+	struct bitfield *tmp2 = bfand(tmp1, right);
+	struct bitfield *tmp3 = bfxor(left, tmp2);
+	bfcpy(tmp3, output);
+	bfdel(tmp1);
+	bfdel(tmp2);
+	bfdel(tmp3);
 }
 
 inline static void eca_89(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
