d399d50eb877527152363691eb37e9c5122462c6
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-03-11 23:28:00 +0900

    Added eca_69()

--- rule30.c
+++ rule30.c
@@ -1002,7 +1002,16 @@ inline static void eca_68(const struct bitfield *left, const struct bitfield *ce
 
 inline static void eca_69(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
 {
-
+	/* ((NOT 1) OR 2 OR 3) XOR 3 */
+	struct bitfield *tmp1 = bfnot(left);
+	struct bitfield *tmp2 = bfor(tmp1, center);
+	struct bitfield *tmp3 = bfor(tmp2, right);
+	struct bitfield *tmp4 = bfxor(tmp3, right);
+	bfcpy(tmp4, output);
+	bfdel(tmp1);
+	bfdel(tmp2);
+	bfdel(tmp3);
+	bfdel(tmp4);
 }
 
 inline static void eca_70(const struct bitfield *left, const struct bitfield *center, const struct bitfield *right, struct bitfield *output)
